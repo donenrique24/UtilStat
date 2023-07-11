f2b2706d199192806d6f2083868b8944730fa277
donenrique24 <mathieu.fortin.re@gmail.com>
2023-07-11 15:55:53 -0400

    Fixing bug in summarizeThisVariable function

--- R/dataDescription.R
+++ R/dataDescription.R
@@ -4,16 +4,19 @@
 #' @param dataset a data.frame object
 #' @param variable the field name (charactar)
 #' @param onlyWherePresent a logical, if true the summary is calculated only for the
+#' @param speciesField the field name that contains the different species
 #' observations where the value is greater than 0 (by default is set to false)
+#' @param format a logical to format the output. The formatting goes as follows: mean (min,max)
+#' @param nbDigits an integer for the number of digits (useless if format = F)
 #'
 #' @export
-summarizeThisVariable <- function(dataset, variable, onlyWherePresent=F) {
+summarizeThisVariable <- function(dataset, variable, speciesField, onlyWherePresent=F, format=F, nbDigits=0) {
   if (onlyWherePresent) {
     .dataset <- dataset[which(dataset[,variable] > 0),]
   } else {
     .dataset <- dataset
   }
-  formula <- stats::as.formula(paste(variable, "speciesGr", sep="~"))
+  formula <- stats::as.formula(paste(variable, speciesField, sep="~"))
   length <- stats::aggregate(formula, .dataset, FUN="length")
   colnames(length)[2] <- paste("n", variable, sep=".")
   mean <- stats::aggregate(formula, .dataset, FUN="mean")
@@ -22,8 +25,13 @@ summarizeThisVariable <- function(dataset, variable, onlyWherePresent=F) {
   colnames(min)[2] <- paste("min", variable, sep=".")
   max <- stats::aggregate(formula, .dataset, FUN="max")
   colnames(max)[2] <- paste("max", variable, sep=".")
-  output <- merge(length, mean, by="speciesGr")
-  output <- merge(output, min, by="speciesGr")
-  output <- merge(output, max, by="speciesGr")
+  output <- merge(length, mean, by=speciesField)
+  output <- merge(output, min, by=speciesField)
+  output <- merge(output, max, by=speciesField)
+  if (format) {
+    output[,3:5] <- format(round(output[,3:5], digit = nbDigits), nsmall = nbDigits)
+    output[,paste("formatted", variable, sep=".")] <- paste0(output[,3], " (", output[,4], ",", output[,5], ")")
+    output <- output[,c(1,2,6)]
+  }
   return(output)
 }
--- tests/testthat/testSimple.R
+++ tests/testthat/testSimple.R
@@ -9,13 +9,17 @@ context("Testing variable description")
 
 example <- UtilStat::exampleRecruitment
 
-a <- summarizeThisVariable(example, "G_TOT")
+a <- summarizeThisVariable(example, "G_TOT", "speciesGr")
+b <- summarizeThisVariable(example, "G_TOT", "speciesGr", format = T, nbDigits = 1)
+
 test_that("Testing results of variable description", {
   expect_equal(a$n.G_TOT, 26248, tolerance = 1E-5)
   expect_equal(a$mean.G_TOT, 16.87469, tolerance = 1E-5)
-  expect_equal(a$min.G_TOT, 0, tolerance = 1E-5)
+  expect_equal(a$min.G_TOT, 0)
   expect_equal(a$max.G_TOT, 64.13471, tolerance = 1E-5)
+  expect_equal(b$formatted.G_TOT, "16.9 (0.0,64.1)")
 })
+
 
 
 context("Testing Hosmer-Lemeshow Implementation")
