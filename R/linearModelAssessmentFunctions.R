ae1e9dcad136c449c6d1c9c1e69e3abcf4e9839b
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2025-05-29 09:23:04 -0400

    Modifying the getResidPlotAgainst function to accept class variables.

--- R/linearModelAssessmentFunctions.R
+++ R/linearModelAssessmentFunctions.R
@@ -18,14 +18,15 @@
 "dataHeight"
 
 #'
-#' Plot average predicted values against classes of a particular covariate
+#' Plot average predicted values against classes of a particular
+#' continuous covariate.
 #'
 #' @param model a model object (e.g. an lme, gls, or lm instance)
 #' @param data the data.frame object used to fit the model
-#' @param classWidth a positive numeric defining the width of the classes of
-#' the variable on the x axis
 #' @param fieldname the name of the variables the average residuals are plotted
 #' against. By default, it is set to "pred" that is the model predictions
+#' @param classWidth a positive numeric defining the width of the classes of
+#' the variable on the x axis
 #' @param printout a logical (true to print out the average residuals)
 #' @param cutoff minimal number of observations for the average residual
 #' to be included in the plot
@@ -33,40 +34,66 @@
 #' use raw residuals
 #'
 #' @export
-getResidPlotAgainst <- function(model, data, classWidth, fieldname = "pred", printout = F, cutoff=NULL, normalize=T) {
+getResidPlotAgainst <- function(model,
+                                data,
+                                fieldname,
+                                classWidth = NULL,
+                                printout = F,
+                                cutoff=NULL,
+                                normalize=T) {
   if (is.null(model) | (!methods::is(model, "gls") & !methods::is(model, "lm"))) {
     stop("The model argument must be a gls or lm instance!")
   }
   if (is.null(data) | !methods::is(data, "data.frame")) {
     stop("The data argument must be the same data.frame object used to fit the model object!")
   }
-  if (is.null(classWidth) | !methods::is(classWidth, "numeric")) {
-    stop("The classWidth argument must be a numeric!")
-  } else if (classWidth <= 0) {
-    stop("The classWidth argument must be a positive numeric!")
+  data <- as.data.frame(data) ## to make sure this is not a data.table
+
+  isClassCovariate <- is.null(classWidth)
+
+  if (!isClassCovariate) {
+    if (!methods::is(classWidth, "numeric")) {
+      stop("If the classWidth is non null, then it must be a numeric!")
+    } else if (classWidth <= 0) {
+      stop("The classWidth argument must be a positive numeric!")
+    }
+  }
+
+  if (!fieldname %in% c(colnames(data), "pred")) {
+    stop("The fieldname should be either pred or one of the fields in the data argument.")
   }
 
   if (fieldname == "pred") {
-    pred <- stats::fitted(model)
-    if (length(pred) != nrow(data)) {
+    var <- stats::fitted(model)
+    if (length(var) != nrow(data)) {
       stop("The number of rows of the data argument does not match the length of the prediction vector!")
     }
-    data$clVar <- round(pred/classWidth + .0001) * classWidth
   } else {
-    data$clVar <- round(data[,fieldname]/classWidth + .0001) * classWidth
+    var <- data[,fieldname]
   }
-  if (normalize) {
-    if (methods::is(model, "lme") | methods::is(model, "gls")) {
-      data$resid <- stats::residuals(model, level=0, type = "normalized")
+
+  if (!isClassCovariate) {
+    if (!methods::is(var, "numeric") & !methods::is(var, "integer")) {
+      stop("The fieldname refers to a field that is not numeric, but the classWidth parameter indicates otherwise.")
     } else {
-      data$resid <- stats::rstandard(model)
+      data$clVar <- round(var/classWidth + .0001) * classWidth
     }
   } else {
-    data$resid <- stats::residuals(model)
+    data$clVar <- var
   }
-  tmp <- stats::aggregate(resid ~ clVar, data, FUN="mean")
-  n <- stats::aggregate(resid ~ clVar, data, FUN="length")
-  colnames(n) <- c("resid", "n")
+
+  if (normalize) {
+    if (methods::is(model, "lme") | methods::is(model, "gls")) {
+      data$res <- stats::residuals(model, level=0, type = "normalized")
+    } else {
+      data$res <- stats::rstandard(model)
+    }
+  } else {
+    data$res <- stats::residuals(model)
+  }
+  tmp <- stats::aggregate(res ~ clVar, data, FUN="mean")
+  n <- stats::aggregate(res ~ clVar, data, FUN="length")
+  colnames(n) <- c("res", "n")
   tmp <- cbind(tmp, n[,"n"])
   if (printout) {
     print(tmp)
@@ -75,12 +102,12 @@ getResidPlotAgainst <- function(model, data, classWidth, fieldname = "pred", pri
   if (!is.null(cutoff)) {
     if (!methods::is(cutoff, "numeric")) {
       warning("The cutoff argument should be a numeric! It will be ignored.")
-      plot <- plot + ggplot2::geom_point(ggplot2::aes(x=clVar, y=resid), tmp, size=2)
+      plot <- plot + ggplot2::geom_point(ggplot2::aes(x=clVar, y=res), tmp, size=2)
     } else {
-      plot <- plot + ggplot2::geom_point(ggplot2::aes(x=clVar, y=resid), tmp[which(tmp$n > cutoff),], size=2)
+      plot <- plot + ggplot2::geom_point(ggplot2::aes(x=clVar, y=res), tmp[which(tmp$n > cutoff),], size=2)
     }
   } else {
-    plot <- plot + ggplot2::geom_point(ggplot2::aes(x=clVar, y=resid), tmp, size=2)
+    plot <- plot + ggplot2::geom_point(ggplot2::aes(x=clVar, y=res), tmp, size=2)
   }
   plot <- plot +
     ggplot2::geom_hline(yintercept = 0, linewidth = 1) +
--- tests/testthat/testLinearModelAssessment.R
+++ tests/testthat/testLinearModelAssessment.R
@@ -8,23 +8,50 @@ context("Testing linear model assessment functions")
 
 require(UtilStat)
 
-fit <- lm(y ~ lnDbh + lnDbh2, UtilStat::dataHeight)
-plot <- UtilStat::getResidPlotAgainst(fit, UtilStat::dataHeight, 1, printout = T, cutoff = 10)
+heightData <- UtilStat::dataHeight
+fit <- lm(y ~ lnDbh + lnDbh2, heightData)
+plot <- UtilStat::getResidPlotAgainst(fit, UtilStat::dataHeight, "pred", 1, printout = T, cutoff = 10)
 
 plotData <- plot$plot_env$tmp
 test_that("Testing underlying data.frame object", {
-  expect_equal(nrow(plotData), 27)
+  expect_equal(nrow(plotData), 26)
   expect_equal(min(plotData$clVar), 3)
   expect_equal(max(plotData$clVar), 31)
-  expect_equal(plotData$resid[1], 0.204873036, tolerance = 1E-8)
+  expect_equal(plotData$res[1], -0.0814085694, tolerance = 1E-8)
 })
 
-plot <- UtilStat::getResidPlotAgainst(fit, UtilStat::dataHeight, fieldname = "BAL", 2, printout = T)
+plot <- UtilStat::getResidPlotAgainst(fit, UtilStat::dataHeight, "BAL", 2, printout = T)
 plotData <- plot$plot_env$tmp
 test_that("Testing underlying data.frame object", {
   expect_equal(nrow(plotData), 40)
   expect_equal(min(plotData$clVar), 0)
   expect_equal(max(plotData$clVar), 112)
-  expect_equal(plotData$resid[1], -0.517593961, tolerance = 1E-8)
+  expect_equal(plotData$res[1], -0.51731305, tolerance = 1E-8)
 })
 
+out <- tryCatch(
+  {
+    UtilStat::getResidPlotAgainst(fit, UtilStat::dataHeight, "USA_CD", 1)
+    return("FAILED - Should have thrown an exception!")
+  },
+  error = function(cond) {
+    return("SUCCESS - Threw an exception as expected!")
+  },
+  warning = function(cond) {
+    return("FAILED - Should have thrown an exception!")
+  }
+)
+
+test_that("Testing if exception was rightfully thrown", {
+  expect_equal(startsWith(out, "SUCCESS"), T)
+})
+
+
+plot <- UtilStat::getResidPlotAgainst(fit, UtilStat::dataHeight, "USA_CD", printout = T)
+plotData <- plot$plot_env$tmp
+test_that("Testing underlying data.frame object", {
+  expect_equal(nrow(plotData), 2)
+  expect_equal(plotData$clVar[1], "HALES")
+  expect_equal(plotData$clVar[2], "LADE2")
+  expect_equal(plotData$res[1], 0.3730116, tolerance = 1E-6)
+})
